06c704a441b912558198d8c435b0cd02b2dbc4c9
tecnicoprogramador18 <jairo95@outloo.com>
2018-05-19 14:25:11 -0600

    de que se puede se puede

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>
+		<?php
+	//declaracion de variables
+			$Pcantidad = 17;
+			$Scantidad = 3;
+			echo $Pcantidad + $Scantidad;
+			$Total = $Pcantidad + $Scantidad;
+			if ($Total >=18){ 
+				echo "Mayor a 18";
+			} else {
+				echo "Menor a 18";
+			}
+		?>
+		</p>
+	</body>
+</html>
